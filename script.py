2c2958ea6dd7ea3652fbd6295dfb978798c1fa4e
audrey <audreyrabenja@gmail.com>
2019-08-17 16:24:54 +0300

    Mon app Django Girls, premier commit

---
+++ script.py
@@ -0,0 +1,20 @@
+print('coucou')
+age=6
+
+if (age < 16 and age > 10):
+     print ("vous êtes adolescent")
+elif (age < 35 and age > 15):
+     print ("vous êtes jeune")
+elif (age < 60 and age > 34):
+    print ("vous êtes adulte")
+elif (age > 60 ):
+    print ("vous êtes vieux")
+def bonjour (name):
+    print ('bonjour ' + name + ' ci-joint votre invitation pour le bal')
+
+
+
+bonjour("audrey")
+bonjour("riantsoa")
+bonjour("hoby")
+bonjour("anja")
